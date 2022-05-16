237c8e541530200a131ed01c20ead52bc37f95c5
Tarsis <tarsis.souza@ifal.edu.br>
2022-05-16 13:54:13 -0300

    Aula 07 - Interfaces Gráficas - 16/05
    Nesta aula foram abordados elementos de interface gráfica por meio do  desenvolvimento de telas semelhantes ao aplicativo Hotel Urbano.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -21,52 +21,116 @@ class _HomePageState extends State<HomePage> {
   }
 
   buildBody() {
-    return ListView(
-      children: [
-        Container(
-          padding: EdgeInsets.all(16),
-          margin: EdgeInsets.all(16),
-          color: Colors.purple,
-          child: Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'TOP DESTINOS MAIS BUSCADOS',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Corre que ta rolando muita promoção',
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                    const SizedBox(height: 8),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Eu quero',
-                        style: TextStyle(color: Colors.black),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            color: Colors.purple,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        'TOP DESTINOS MAIS BUSCADOS',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
                       ),
-                      style:
-                          ElevatedButton.styleFrom(primary: Color(0xFFF8FF04)),
-                    )
-                  ],
+                      Text(
+                        'Corre que ta rolando muita promoção',
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                      const SizedBox(height: 8),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Eu quero',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color(0xFFF8FF04)),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(width: 24),
-              const Placeholder(
-                fallbackHeight: 150,
-                fallbackWidth: 100,
-                color: Colors.white,
-              )
-            ],
+                const SizedBox(width: 24),
+                const Placeholder(
+                  fallbackHeight: 150,
+                  fallbackWidth: 100,
+                  color: Colors.white,
+                )
+              ],
+            ),
           ),
-        )
-      ],
+          SizedBox(height: 16),
+          Card(
+            child: Column(
+              children: [
+                Placeholder(fallbackHeight: 150),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Pacote Cancún 2021',
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text('Aéreo - Hotel All incluse '),
+                      SizedBox(height: 8),
+                      Row(
+                        children: const [
+                          Icon(Icons.wb_sunny_outlined),
+                          SizedBox(width: 4),
+                          Text('05 Diárias'),
+                          SizedBox(width: 8),
+                          Icon(Icons.person_outline),
+                          SizedBox(width: 4),
+                          Text('01 Pessoa'),
+                        ],
+                      ),
+                      SizedBox(height: 8),
+                      Text('A partir de R\$ 6816'),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'R\$ 3.749',
+                            style: TextStyle(
+                              fontSize: 28,
+                              color: Colors.orange,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          Text('Taxa Grátis em até 12x'),
+                        ],
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Cancelamento Grátis',
+                        style: TextStyle(
+                            color: Colors.green[700],
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
